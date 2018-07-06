c075c51d2b641621da38245c3bd72f7b55d3c07d
jishincreo <jishin@creoit.com>
2018-07-06 12:26:48 +0530

    created notification channel

--- app/src/main/java/com/jishindev/bdayalarm/NotifyHelper.kt
+++ app/src/main/java/com/jishindev/bdayalarm/NotifyHelper.kt
@@ -1,6 +1,7 @@
 package com.jishindev.bdayalarm
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.ComponentName
@@ -11,6 +12,7 @@ import android.graphics.Color
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import android.support.v4.app.AlarmManagerCompat
 import android.support.v4.app.NotificationCompat
 import android.util.Log
@@ -20,7 +22,7 @@ import java.util.*
 
 
 const val TIME_TO_SURPRISE = "07-JUL-2018 00:15:00"
-lateinit var ringtoneAlarm:Ringtone
+lateinit var ringtoneAlarm: Ringtone
 
 fun Context.setSurprise(dateTime: String = TIME_TO_SURPRISE) {
     Log.d("NotifyHelper", "setSurprise() called")
@@ -51,8 +53,8 @@ fun Context.showSurprise() {
     val pIntent = PendingIntent.getActivity(this, 0, intent, 0)
     val message = "Hey Anju, Sarath is planning something for you. Let's go?"
     val notification = NotificationCompat.Builder(this, "default")
-             //.setTicker("BDay")
-             .setContentTitle("Surprise")
+            //.setTicker("BDay")
+            .setContentTitle("Surprise")
             .setColor(Color.RED)
             .setContentIntent(pIntent)
             .setContentText(message)
@@ -64,6 +66,7 @@ fun Context.showSurprise() {
             .setOngoing(true)
             .build()
 
+
     val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     nm.notify(1, notification)
     Log.d("NotifyHelper", "onReceive: Surpriseeeee!!!")
@@ -72,4 +75,18 @@ fun Context.showSurprise() {
     val alarmTone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
     ringtoneAlarm = RingtoneManager.getRingtone(applicationContext, alarmTone)
     ringtoneAlarm.play()
+}
+
+fun Context.createNotificationChannel() {
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = "default"
+        val description = "All default notifications"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(name, name, importance)
+        channel.description = description
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
 }
